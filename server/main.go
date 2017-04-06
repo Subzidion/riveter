08aaccef69b06c9ebe5a8a38e930527fd1a038c1
Carl 'Subzidion' Hiltbrunner <carlhiltbrunner@hotmail.com>
2017-04-06 15:48:32 -0400

    Fix OPTIONS handling.

--- server/main.go
+++ server/main.go
@@ -51,7 +51,13 @@ func CORSMiddleware() gin.HandlerFunc {
   return func(c *gin.Context) {
     c.Writer.Header().Set("Content-Type", "application/json")
     c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-    c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, Authorization")
+    c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+    if c.Request.Method == "OPTIONS" {
+      log.Println("OPTIONS")
+      c.AbortWithStatus(200)
+    } else {
+      c.Next()
+    }
   }
 }
 
